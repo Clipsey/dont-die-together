483bb3f5adc252de5b28e74d343220c5c509bf41
Clipsey <20288304+Clipsey@users.noreply.github.com>
2019-09-23 12:14:00 -0700

    configuration

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,8 @@
-if (process.env.NODE_ENV === 'production') {
+console.log("process.env.NODE_ENV", process.env.NODE_ENV);
+console.log("==========================================");
+// console.log("")
+let env = process.env.NODE_ENV || 'development';
+if (env === 'production') {
   module.exports = require('./keys_prod');
 } else {
   module.exports = require('./keys_dev');
