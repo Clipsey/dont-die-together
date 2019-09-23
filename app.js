aace7ae698f9645793401a4b71bf153e6d84581b
Clipsey <20288304+Clipsey@users.noreply.github.com>
2019-09-23 10:53:17 -0700

    remove global socket frontend, change server

--- app.js
+++ app.js
@@ -3,11 +3,14 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const passport = require('passport');
 const path = require('path');
+const socketIO = require('socket.io');
+const http = require('http');
 
 const users = require('./routes/api/users');
 const tweets = require('./routes/api/tweets');
 
 const app = express();
+const server = http.createServer(app);
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -21,7 +24,7 @@ mongoose
   .catch(err => console.log(err));
 
 const port = process.env.PORT || 5000;
-app.listen(port, () => console.log(`Server is running on port ${port}`));
+// app.listen(port, () => console.log(`Server is running on port ${port}`));
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('frontend/build'));
@@ -30,9 +33,22 @@ if (process.env.NODE_ENV === 'production') {
   })
 }
 
-
 app.use('/api/users', users);
 app.use('/api/tweets', tweets);
+
+
+// Websocket io connections
+const io = socketIO(server);
+
+io.on('connection', socket => {
+  console.log('User connected');
+
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  })
+})
+
+server.listen(port);
 
 // app.get("/", (req, res) => {
 //   res.send("Hello World")
