3943d36b78a5d5fb0816b181c4081d2e3f67b418
Clipsey <20288304+Clipsey@users.noreply.github.com>
2019-09-23 14:33:42 -0700

    With heroku config

--- config/passport.js
+++ config/passport.js
@@ -7,8 +7,8 @@ const keys = require('./keys');
 
 const options = {};
 options.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
-keys.secretOrKey == null ? options.secretOrKey = process.env.SECRET : options.secretOrKey = keys.secretOrKey;
-// options.secretOrKey = keys.secretOrKey;
+// keys.secretOrKey == null ? options.secretOrKey = process.env.SECRET : options.secretOrKey = keys.secretOrKey;
+options.secretOrKey = keys.secretOrKey;
 
 module.exports = passport => {
   passport.use(new JwtStrategy(options, (jwt_payload, done) => {
