207bf8fdd6cb48360c09256380da074e1b25d8f4
Clipsey <20288304+Clipsey@users.noreply.github.com>
2019-09-24 09:22:34 -0700

    Util sockets

--- frontend/src/components/root.js
+++ frontend/src/components/root.js
@@ -4,10 +4,29 @@ import { HashRouter } from 'react-router-dom';
 // We'll create this soon
 import App from './app';
 
+import socketIOClient from 'socket.io-client';
+import {emitSetup, onSetup} from '../util/sockets_util';
+
+let socket;
+let endpoint = 'localhost:5000';
+if (process.env.NODE_ENV === 'development') {
+  socket = socketIOClient(endpoint);
+} else {
+  socket = socketIOClient();
+}
+
+const emit = emitSetup(socket);
+const subscribe = onSetup(socket);
+
+// let socketMethods = {
+//   emit,
+//   on
+// }
+
 const Root = ({ store }) => (
   <Provider store={store}>
     <HashRouter>
-      <App />
+      <App emit={emit} subscribe={subscribe}/>
     </HashRouter>
   </Provider>
 );
