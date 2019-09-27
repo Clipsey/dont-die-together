e1cb266bc3c8814c688bf402c5b171270f68b553
nairie <wesleytam@yahoo.com>
2019-09-27 15:59:04 -0700

    fixing heroku

--- app.js
+++ app.js
@@ -28,16 +28,17 @@ mongoose
 const port = process.env.PORT || 5000;
 // app.listen(port, () => console.log(`Server is running on port ${port}`));
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('frontend/build'));
-  app.get('/*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
-  })
-}
-
 app.use('/api/users', users);
 app.use('/api/tweets', tweets);
 
+// moved these down
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('frontend/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+  })
+}
 
 // Websocket io connections
 const io = socketIO(server);
